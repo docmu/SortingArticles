5c27ec9f00dfd46484a62e300ad750acf4123686
docmu <docm@mymail.vcu.edu>
2020-12-23 18:57:27 -0500

    functional component implementation

--- src/App.js
+++ src/App.js
@@ -1,56 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 import 'h8k-components';
 
 import Articles from './components/Articles';
 
 const title = "Sorting Articles";
-class App extends Component {
-    //the articles array passed through props sorted by highest upvotes by default
-    articles = this.props.articles.sort((a, b) => b.upvotes - a.upvotes);
-    //state of the button clicked
-    state = {
-        sortByUpvote: false,
-        sortByRecent: false,
-    };
-    mostUpvotedClick = () => {
-        this.setState({ sortByUpvote: !this.state.sortByUpvote });
-        //sort by upvotes in descending order
-        this.articles.sort((a, b) => b.upvotes - a.upvotes);
-    }
-    mostRecentClick = () => {
-        this.setState({ sortByRecent: !this.state.sortByRecent });
-        //sort by date in descending order
-        this.articles.sort((a, b) => new Date(b.date) - new Date(a.date));
-    }
-    render() {
-        return (
-            <div className="App">
-                <h8k-navbar header={title}></h8k-navbar>
-                <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                    <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                    <button onClick={this.mostUpvotedClick} data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                    <button onClick={this.mostRecentClick} data-testid="most-recent-link" className="small">Most Recent</button>
-                </div>
-                {/* for every article in the articles array, render an <Articles /> component */}
-                {this.articles.map(article => <Articles key={article.title} title={article.title} upvotes={article.upvotes} date={article.date} />)}
-            </div>
-        );
-    }
-}
 
-// function App({ articles }) {
-//     return (
-//         <div className="App">
-//             <h8k-navbar header={title}></h8k-navbar>
-//             <div className="layout-row align-items-center justify-content-center my-20 navigation">
-//                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-//                 <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-//                 <button data-testid="most-recent-link" className="small">Most Recent</button>
+// export default class App extends Component {
+//     //state of the button clicked
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             sortByUpvote: false,
+//             sortByRecent: false,
+//         };
+//     };
+//     componentDidMount() {
+//         //sorted by most upvoted by default
+//         this.mostUpvotedClick();
+//     }
+//     mostUpvotedClick() {
+//         this.setState({ sortByUpvote: !this.state.sortByUpvote });
+//         //sort by upvotes in descending order
+//         this.props.articles.sort((a, b) => b.upvotes - a.upvotes);
+//     }
+//     mostRecentClick = () => {
+//         this.setState({ sortByRecent: !this.state.sortByRecent });
+//         //sort by date in descending order
+//         this.props.articles.sort((a, b) => new Date(b.date) - new Date(a.date));
+//     }
+//     render() {
+//         return (
+//             <div className="App">
+//                 <h8k-navbar header={title}></h8k-navbar>
+//                 <div className="layout-row align-items-center justify-content-center my-20 navigation">
+//                     <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
+//                     <button onClick={() => this.mostUpvotedClick()} data-testid="most-upvoted-link" className="small">Most Upvoted</button>
+//                     <button onClick={this.mostRecentClick} data-testid="most-recent-link" className="small">Most Recent</button>
+//                 </div>
+//                 <Articles articles={this.props.articles} />
 //             </div>
-//             <Articles articles={articles} />
-//         </div>
-//     );
+//         );
+//     }
 // }
 
-export default App;
+export default function App({ articles }) {
+    const [buttonClicked, setButtonClicked] = useState(-1);
+    const mostUpvotedClick = () => {
+        console.log('most upvoted');
+        //0 represents Most Upvoted button
+        setButtonClicked(0);
+        //sort by upvotes in descending order
+        articles.sort((a, b) => b.upvotes - a.upvotes);
+    }
+    const mostRecentClick = () => {
+        console.log('most recent');
+        //1 represents Most Recent button
+        setButtonClicked(1);
+        //sort by date in descending order
+        articles.sort((a, b) => new Date(b.date) - new Date(a.date));
+    }
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        //-1 represents the page on the first render
+        if (buttonClicked === -1) {
+            //list needs to be sorted by most upvoted by default
+            mostUpvotedClick();
+        }
+    });
+    return (
+        <div className="App">
+            <h8k-navbar header={title}></h8k-navbar>
+            <div className="layout-row align-items-center justify-content-center my-20 navigation">
+                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
+                <button onClick={() => mostUpvotedClick()} data-testid="most-upvoted-link" className="small">Most Upvoted</button>
+                <button onClick={() => mostRecentClick()} data-testid="most-recent-link" className="small">Most Recent</button>
+            </div>
+            <Articles articles={articles} />
+        </div>
+    );
+}
+
+
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Articles(props) {
+export default function Articles({ articles }) {
     return (
         <div className="card w-50 mx-auto">
             <table>
@@ -11,40 +11,41 @@ function Articles(props) {
                         <th>Date</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr data-testid="article" key="article-index">
-                        <td data-testid="article-title">{props.title}</td>
-                        <td data-testid="article-upvotes">{props.upvotes}</td>
-                        <td data-testid="article-date">{props.date}</td>
-                    </tr>
-                </tbody>
+                {articles.map(article =>
+                    <tbody key={article.title}>
+                        <tr data-testid="article" key="article-index">
+                            <td data-testid="article-title">{article.title}</td>
+                            <td data-testid="article-upvotes">{article.upvotes}</td>
+                            <td data-testid="article-date">{article.date}</td>
+                        </tr>
+                    </tbody>
+                )}
             </table>
         </div>
     );
-
 }
-// function Articles() {
+
+// export default function Articles(props) {
 //     return (
 //         <div className="card w-50 mx-auto">
 //             <table>
 //                 <thead>
-//                 <tr>
-//                     <th>Title</th>
-//                     <th>Upvotes</th>
-//                     <th>Date</th>
-//                 </tr>
+//                     <tr>
+//                         <th>Title</th>
+//                         <th>Upvotes</th>
+//                         <th>Date</th>
+//                     </tr>
 //                 </thead>
-//                 <tbody>
-//                 <tr data-testid="article" key="article-index">
-//                     <td data-testid="article-title">Article 1 title</td>
-//                     <td data-testid="article-upvotes">Article 1 title</td>
-//                     <td data-testid="article-date">Article 1 title</td>
-//                 </tr>
-//                 </tbody>
+//                 {props.articles.map(article =>
+//                     <tbody key={article.title}>
+//                         <tr data-testid="article" key="article-index">
+//                             <td data-testid="article-title">{article.title}</td>
+//                             <td data-testid="article-upvotes">{article.upvotes}</td>
+//                             <td data-testid="article-date">{article.date}</td>
+//                         </tr>
+//                     </tbody>
+//                 )}
 //             </table>
 //         </div>
 //     );
-
 // }
-
-export default Articles;
