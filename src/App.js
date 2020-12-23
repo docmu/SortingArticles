b94458d721e210de93cf1aa51709485dfcc0e686
docmu <docm@mymail.vcu.edu>
2020-12-23 17:36:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'h8k-components';
 
 import Articles from './components/Articles';
 
 const title = "Sorting Articles";
-
-function App({articles}) {
-
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+class App extends Component {
+    //the articles array passed through props sorted by highest upvotes by default
+    articles = this.props.articles.sort((a, b) => b.upvotes - a.upvotes);
+    //state of the button clicked
+    state = {
+        sortByUpvote: false,
+        sortByRecent: false,
+    };
+    mostUpvotedClick = () => {
+        this.setState({ sortByUpvote: !this.state.sortByUpvote });
+        //sort by upvotes in descending order
+        this.articles.sort((a, b) => b.upvotes - a.upvotes);
+    }
+    mostRecentClick = () => {
+        this.setState({ sortByRecent: !this.state.sortByRecent });
+        //sort by date in descending order
+        this.articles.sort((a, b) => new Date(b.date) - new Date(a.date));
+    }
+    render() {
+        return (
+            <div className="App">
+                <h8k-navbar header={title}></h8k-navbar>
+                <div className="layout-row align-items-center justify-content-center my-20 navigation">
+                    <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
+                    <button onClick={this.mostUpvotedClick} data-testid="most-upvoted-link" className="small">Most Upvoted</button>
+                    <button onClick={this.mostRecentClick} data-testid="most-recent-link" className="small">Most Recent</button>
+                </div>
+                {/* for every article in the articles array, render an <Articles /> component */}
+                {this.articles.map(article => <Articles key={article.title} title={article.title} upvotes={article.upvotes} date={article.date} />)}
             </div>
-            <Articles articles={articles}/>
-        </div>
-    );
-
+        );
+    }
 }
+
+// function App({ articles }) {
+//     return (
+//         <div className="App">
+//             <h8k-navbar header={title}></h8k-navbar>
+//             <div className="layout-row align-items-center justify-content-center my-20 navigation">
+//                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
+//                 <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
+//                 <button data-testid="most-recent-link" className="small">Most Recent</button>
+//             </div>
+//             <Articles articles={articles} />
+//         </div>
+//     );
+// }
 
 export default App;
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,28 +1,50 @@
 import React from 'react';
 
-function Articles() {
-
+function Articles(props) {
     return (
         <div className="card w-50 mx-auto">
             <table>
                 <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
+                    <tr>
+                        <th>Title</th>
+                        <th>Upvotes</th>
+                        <th>Date</th>
+                    </tr>
                 </thead>
                 <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
+                    <tr data-testid="article" key="article-index">
+                        <td data-testid="article-title">{props.title}</td>
+                        <td data-testid="article-upvotes">{props.upvotes}</td>
+                        <td data-testid="article-date">{props.date}</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
     );
 
 }
+// function Articles() {
+//     return (
+//         <div className="card w-50 mx-auto">
+//             <table>
+//                 <thead>
+//                 <tr>
+//                     <th>Title</th>
+//                     <th>Upvotes</th>
+//                     <th>Date</th>
+//                 </tr>
+//                 </thead>
+//                 <tbody>
+//                 <tr data-testid="article" key="article-index">
+//                     <td data-testid="article-title">Article 1 title</td>
+//                     <td data-testid="article-upvotes">Article 1 title</td>
+//                     <td data-testid="article-date">Article 1 title</td>
+//                 </tr>
+//                 </tbody>
+//             </table>
+//         </div>
+//     );
+
+// }
 
 export default Articles;
